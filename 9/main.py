cee98442d98c4cf9f3cc72fee7d7cd1d22e15a29
Rui Almeida <rui.almeida011@gmail.com>
2022-12-09 14:17:08 +0000

    Day 9 boilerplate + inputs

---
+++ 9/main.py
@@ -0,0 +1,20 @@
+def part_one(input_file):
+    pass
+
+def part_two(input_file):
+    pass
+
+def main():
+    test = "input_test.txt"
+    input = "input.txt"
+
+    assert part_one(test) == 13
+    print(f"Total part 1 test: {part_one(test)}")
+    # print(f"Total part 1 input: {part_one(input)}")
+
+    # assert part_two(test) == 8
+    # print(f"Total part 2 test: {part_two(test)}")
+    # print(f"Total part 2 input: {part_two(input)}")
+
+if __name__ == '__main__':
+    main()
