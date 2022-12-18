4d0bd9aa7bd7f0b3923274b421d43818a09a7f28
Rui Almeida <rui.almeida011@gmail.com>
2022-12-18 09:49:59 +0000

    Day 17... tetris uh..

---
+++ 17/main.py
@@ -0,0 +1,19 @@
+def part_ont(input_file):
+    pass
+
+
+def main():
+    test = "input_test.txt"
+    input = "input.txt"
+
+    #assert part_one(test) == 3068
+    print(f"Total part 1 test: {part_one(test)}")
+    # print(f"Total part 1 input: {part_one(input)}")
+
+    # assert part_two(test) == 140
+    # print(f"Total part 2 test: {part_two(test)}")
+    # print(f"Total part 2 input: {part_two(input)}")
+
+
+if __name__ == 'main':
+    main()
