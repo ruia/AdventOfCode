3fdeac3e75f80cb2f1689d05245995f86b956cfd
Rui Almeida <rui.almeida011@gmail.com>
2022-12-25 18:04:33 +0000

    Day 25 part one snafud

--- 25/main.py
+++ 25/main.py
@@ -1,30 +1,37 @@
 def part_one(input_file):
     sum_decimal = 0
 
+    fuels = { '=': -2, '-': -1, '0': 0, '1': 1, '2': 2 }
+    decimals = dict(map(reversed, fuels.items()))
+
     with open(input_file) as f:
         for line in f:
             line = line.strip()
             for i, char in enumerate(line):
-                if char == '=':
-                    val = -2
-                elif char == '-':
-                    val = -1
-                else:
-                    val = int(char)
-                
-                sum_decimal += val * pow(5, len(line)-i-1)
+                sum_decimal += fuels[char] * pow(5, len(line)-i-1)
             
-        return sum_decimal
-        
+        ret = []
+
+        while sum_decimal > 0:
+            remain = sum_decimal % 5
+            if remain > 2:
+                sum_decimal += remain
+                ret.append(decimals[remain - 5])
+            else:
+                ret.append(str(remain))
+
+            sum_decimal //= 5    
+
+    return ''.join(reversed(ret))
+    
 
 def main():
     test = "input_test.txt"
     input = "input.txt"
-    # test = "input_test_min.txt"
 
-    # assert part_one(test) == '2=-1=0'
+    assert part_one(test) == '2=-1=0'
     print(f"Total part 1 test: {part_one(test)}")
-    # print(f"Total part 1 input: {part_one(input)}")
+    print(f"Total part 1 input: {part_one(input)}")
 
     # assert part_two(test) == 93
     # print(f"Total part 2 test: {part_two(test)}")
